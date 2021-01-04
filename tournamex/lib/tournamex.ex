9facfd5afde53322622f02e7c6aa19f2dd15e02f
Papillon6814 <so1rou.k@gmail.com>
2021-01-05 00:15:55 +0900

    feat: create a project base

---
+++ tournamex/lib/tournamex.ex
@@ -0,0 +1,18 @@
+defmodule Tournamex do
+  @moduledoc """
+  Documentation for Tournamex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tournamex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ tournamex/test/tournamex_test.exs
@@ -0,0 +1,8 @@
+defmodule TournamexTest do
+  use ExUnit.Case
+  doctest Tournamex
+
+  test "greets the world" do
+    assert Tournamex.hello() == :world
+  end
+end
