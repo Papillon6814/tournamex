0438b3ea7052524f6092022baeb6d06165c15006
Papillon6814 <so1rou.k@gmail.com>
2021-01-05 01:44:52 +0900

    hexdocsに公開

--- tournamex/lib/tournamex.ex
+++ tournamex/lib/tournamex.ex
@@ -1,18 +1,43 @@
 defmodule Tournamex do
   @moduledoc """
-  Documentation for Tournamex.
+  # Tournamex
+  Simple package for managing online tournament system.
+  Tournamex only has a function for generating matchlist from list.
+  We will add functions gradually.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Tournamex.hello()
-      :world
-
+  Generates a matchlist.
   """
-  def hello do
-    :world
+  def generate_matchlist(list) do
+    unless is_list(list) do
+      {:error, "Argument is not list"}
+    else
+      list
+      |> generate()
+      |> case do
+        list when is_list(list) -> {:ok, list}
+        tuple when is_tuple(tuple) -> tuple
+        scala -> {:ok, [scala]}
+      end
+    end
   end
+
+  defp generate(list) when list != [] do
+    shuffled = list |> Enum.shuffle()
+    case(length(shuffled)) do
+    1 ->
+      shuffled |> hd()
+    2 -> shuffled
+    _ ->
+      b = Enum.slice(shuffled, 0..trunc(length(shuffled)/2 -1))
+      |> generate()
+
+      c = Enum.slice(shuffled, trunc(length(shuffled)/2)..length(shuffled)-1)
+      |> generate()
+
+      [b,c]
+    end
+  end
+  defp generate([]), do: {:error, "No entrants"}
 end
--- tournamex/mix.exs
+++ tournamex/mix.exs
@@ -6,23 +6,30 @@ defmodule Tournamex.MixProject do
       app: :tournamex,
       version: "0.1.0",
       elixir: "~> 1.9",
+      description: "Simple package for managing tournament.",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.22.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Papillon6814"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/Papillon6814/tournamex"}
     ]
   end
 end
