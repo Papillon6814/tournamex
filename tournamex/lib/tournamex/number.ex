f6243b9b44fe8288edf1d22d119a1ef3ed68f171
Papillon6814 <so1rou.k@gmail.com>
2021-01-06 00:39:59 +0900

    docs: ドキュメンテーションコメントの追加

--- tournamex/lib/tournamex/number.ex
+++ tournamex/lib/tournamex/number.ex
@@ -1,4 +1,13 @@
 defmodule Tournamex.Number do
+  @moduledoc """
+  # Number
+  Numbers for managing a tournament.
+  """
+
+  @doc """
+  Finds a closest number to power of 2.
+  It is used for presenting tournament brackets.
+  """
   def closest_number_to_power_of_two(num, acc \\ 2) do
     if num > acc do
       closest_number_to_power_of_two(num, acc * 2)
