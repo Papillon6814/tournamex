8a70f3b90f4cd9eb81738f9b628d62451d6a7e6b
Papillon6814 <so1rou.k@gmail.com>
2021-01-05 20:04:38 +0900

    feat: add delete_loser

--- tournamex/lib/tournamex.ex
+++ tournamex/lib/tournamex.ex
@@ -42,10 +42,40 @@ defmodule Tournamex do
   defp generate([]), do: {:error, "No entrants"}
 
   @doc """
+  Delete losers from match list.
+  """
+  def delete_loser(list, loser) when is_integer(loser) do
+    delete_loser(list, [loser])
+  end
+
+  def delete_loser([a, b], loser) when is_integer(a) and is_integer(b) do
+    list = [a, b] -- loser
+    if length(list) == 1, do: hd(list), else: list
+  end
+
+  def delete_loser(list, loser) do
+    case list do
+      [[a, b], [c, d]] ->
+        [delete_loser([a, b], loser), delete_loser([c, d], loser)]
+      [a, [b, c]] when is_integer(a) and [a] == loser ->
+        [b, c]
+      [a, [b, c]] ->
+        [a, delete_loser([b, c], loser)]
+      [[a, b], c] when is_integer(c) and [c] == loser ->
+        [a, b]
+      [[a, b], c] ->
+        [delete_loser([a, b], loser), c]
+      [a, b] ->
+        delete_loser([a, b], loser)
+      _ -> raise "Bad Argument"
+    end
+  end
+
+  @doc """
   Returns data which is presenting tournament brackets.
   """
   def brackets(match_list) do
-    align(match_list)
+    {:ok, align(match_list)}
   end
 
   defp align(match_list, result \\ []) do
@@ -72,5 +102,4 @@ defmodule Tournamex do
       end
     end)
   end
-
 end
--- tournamex/test/tournamex_test.exs
+++ tournamex/test/tournamex_test.exs
@@ -23,37 +23,89 @@ defmodule TournamexTest do
   describe "brackets" do
     test "brackets/1 works fine with valid list data of size 3" do
       match_list = [3, [1, 2]]
-      assert Tournamex.brackets(match_list) == [[2, 1], [nil, 3]]
+      assert Tournamex.brackets(match_list) == {:ok, [[2, 1], [nil, 3]]}
     end
 
     test "brackets/1 works fine with valid list data of size 4" do
       match_list = [[1, 2], [3, 4]]
-      assert Tournamex.brackets(match_list) == [[4, 3], [2, 1]]
+      assert Tournamex.brackets(match_list) == {:ok, [[4, 3], [2, 1]]}
     end
 
     test "brackets/1 works fine with valid list data of size 5" do
       match_list = [[1, 2], [3, [4, 5]]]
-      assert Tournamex.brackets(match_list) == [[5, 4], [nil, 3], [2, 1]]
+      assert Tournamex.brackets(match_list) == {:ok, [[5, 4], [nil, 3], [2, 1]]}
     end
 
     test "brackets/1 works fine with valid list data of size 6" do
       match_list = [[1, [2, 3]], [4, [5, 6]]]
-      assert Tournamex.brackets(match_list) == [[6, 5], [nil, 4], [3, 2], [nil, 1]]
+      assert Tournamex.brackets(match_list) == {:ok, [[6, 5], [nil, 4], [3, 2], [nil, 1]]}
     end
 
     test "brackets/1 works fine with valid list data of size 7" do
       match_list = [[1, [2, 3]], [[4, 5], [6, 7]]]
-      assert Tournamex.brackets(match_list) == [[7, 6], [5, 4], [3, 2], [nil, 1]]
+      assert Tournamex.brackets(match_list) == {:ok, [[7, 6], [5, 4], [3, 2], [nil, 1]]}
     end
 
     test "brackets/1 works fine with valid list data of size 8" do
       match_list = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
-      assert Tournamex.brackets(match_list) == [[8, 7], [6, 5], [4, 3], [2, 1]]
+      assert Tournamex.brackets(match_list) == {:ok, [[8, 7], [6, 5], [4, 3], [2, 1]]}
     end
 
     test "brackets/1 works fine with valid list data of size 9" do
       match_list = [[[1, 2], [3, 4]], [[5, 6], [7, [8, 9]]]]
-      assert Tournamex.brackets(match_list) == [[9, 8], [nil, 7], [6, 5], [4, 3], [2, 1]]
+      assert Tournamex.brackets(match_list) == {:ok, [[9, 8], [nil, 7], [6, 5], [4, 3], [2, 1]]}
+    end
+  end
+
+  describe "delete loser" do
+    test "delete_loser/2 works fine with a valid data of 4 players" do
+      list = [[1, 2], [3, 4]]
+      assert Tournamex.delete_loser(list, 1) == [2, [3, 4]]
+      assert Tournamex.delete_loser(list, [1]) == [2, [3, 4]]
+      assert Tournamex.delete_loser(list, 2) == [1, [3, 4]]
+      assert Tournamex.delete_loser(list, [2]) == [1, [3, 4]]
+      assert Tournamex.delete_loser(list, 3) == [[1, 2], 4]
+      assert Tournamex.delete_loser(list, [3]) == [[1, 2], 4]
+      assert Tournamex.delete_loser(list, 4) == [[1, 2], 3]
+      assert Tournamex.delete_loser(list, [4]) == [[1, 2], 3]
+    end
+
+    test "delete_loser/2 works fine with a valid data of 3 players" do
+      list = [[1, 2], 3]
+
+      assert Tournamex.delete_loser(list, 1) == [2, 3]
+      assert Tournamex.delete_loser(list, [1]) == [2, 3]
+      assert Tournamex.delete_loser(list, 2) == [1, 3]
+      assert Tournamex.delete_loser(list, [2]) == [1, 3]
+      assert Tournamex.delete_loser(list, 3) == [1, 2]
+      assert Tournamex.delete_loser(list, [3]) == [1, 2]
+    end
+
+    test "delete_loser/2 works fine with a valid data of 5 players" do
+      list = [[1, 2], [[3, 4], 5]]
+
+      assert Tournamex.delete_loser(list, 1) == [2, [[3, 4], 5]]
+      assert Tournamex.delete_loser(list, [1]) == [2, [[3, 4], 5]]
+      assert Tournamex.delete_loser(list, 2) == [1, [[3, 4], 5]]
+      assert Tournamex.delete_loser(list, [2]) == [1, [[3, 4], 5]]
+      assert Tournamex.delete_loser(list, 3) == [[1, 2], [4, 5]]
+      assert Tournamex.delete_loser(list, [3]) == [[1, 2], [4, 5]]
+      assert Tournamex.delete_loser(list, 4) == [[1, 2], [3, 5]]
+      assert Tournamex.delete_loser(list, [4]) == [[1, 2], [3, 5]]
+      assert Tournamex.delete_loser(list, 5) == [[1, 2], [3, 4]]
+      assert Tournamex.delete_loser(list, [5]) == [[1, 2], [3, 4]]
+    end
+
+    test "delete_loser/2 does not work with an invalid data of 3 players" do
+      list = [1, 2, 3]
+
+      assert catch_error(Tournamex.delete_loser(list, 1)) == %RuntimeError{message: "Bad Argument"}
+    end
+
+    test "delete_loser/2 does not work with an invalid data of 1 player" do
+      list = [1]
+
+      assert catch_error(Tournamex.delete_loser(list, 1)) == %RuntimeError{message: "Bad Argument"}
     end
   end
 end
